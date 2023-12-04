3614c5bfa855f1a8368e58832e2de5d4ae8e73bb
Kaan <kaan.oklu@hotmail.com>
2023-12-04 22:25:11 +0300

    create test nextjs projects

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+       Hey This is invoices page
+    </div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+       Hey This is dashboard page
+    </div>
+  )
+}
+
+export default Page
